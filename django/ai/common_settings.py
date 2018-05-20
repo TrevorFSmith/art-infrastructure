8e46244048a70b53919fcdf83e6f51e8b3d73639
Bogdan Kulbida <info@konstankino.com>
2018-05-20 01:13:08 -0400

    Remove flock.

--- django/ai/common_settings.py
+++ django/ai/common_settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'lighting',
     'weather',
     'iboot',
-    'flock'
 ]
 
 MIDDLEWARE = [
--- django/flock/__init__.py
+++
@@ -1,2 +0,0 @@
-"""The app which provides the backend for the status listener, 
-which makes a simple testing protocol available to the art technician."""
